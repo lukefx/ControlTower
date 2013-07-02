fd47f02aa2dacb291e3eadf58fffb44778ed96a2
Luca Simone <luca.simone@lugano.ch>
2013-07-02 10:50:25 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Controltower::Application.config.secret_key_base = '956d419827c50af251a5a8debbced666b8aef9ee67dea1d372a805176859caad0bf32840621ea580739f99f2a7017b13c8e63735e94cfc108f1e1e4537a6affa'
